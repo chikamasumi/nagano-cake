c6bfd494680fd81bc036275ed2a4e3b8cd217b55
EC2 Default User <ec2-user@ip-172-31-60-190.ec2.internal>
2021-08-31 10:07:38 +0000

    [Add]totalprice

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -3,6 +3,7 @@ class Public::CartItemsController < ApplicationController
   def index
     @cart_items = CartItem.all
     @cart_item = CartItem.new
+    @total_price = 0
   end
 
   def create
@@ -12,16 +13,14 @@ class Public::CartItemsController < ApplicationController
       #flash[:notice] = "カートに追加しました"
       #redirect_to cart_items_path
     #end
-    @cart_item = current_customer.cart_items.build(cart_item_params)
-    @cart_items = current_customer.cart_items.all
-    @cart_items.each do |cart_item|
-      if cart_item.item_id == @cart_item.item_id
-        new_amount = cart_item.amount + @cart_item.amount
-        cart_item.update_attribute(:amount, new_amount)
-        @cart_item.delete
-      end
+    item = CartItem.new(cart_item_params)
+    cart_item = current_customer.cart_items.find_by(item_id: item.item_id)
+    if cart_item.present?
+      new_amount = cart_item.amount + item.amount
+      cart_item.update_attribute(:amount, new_amount)
+    else
+      item.save
     end
-    @cart_item.save
     redirect_to cart_items_path
   end
 
@@ -32,13 +31,13 @@ class Public::CartItemsController < ApplicationController
   end
 
   def destroy_all
-    CartItem.destroy_all
+    current_customer.cart_items.destroy_all
     redirect_to cart_items_path
   end
 
   private
   def cart_item_params
-    params.permit(:item_id, :customer_id, :amount)
+    params.require(:cart_item).permit(:item_id, :customer_id, :amount)
   end
 
 end
--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -13,13 +13,14 @@ class Public::OrdersController < ApplicationController
     @order.save
     @cart_items = current_customer.cart_items
     @cart_items.each do |cart_item|
-    @order_ditail = OrderDitail.new
-    @order_ditail.order_id = @order.id
-    @order_ditail.item_id = cart_item.item.id
-    @order_ditail.price = cart_item.item.price
-    @order_ditail.amount = cart_item.amount
-    @order_ditail.save
+      @order_ditail = OrderDitail.new
+      @order_ditail.order_id = @order.id
+      @order_ditail.item_id = cart_item.item.id
+      @order_ditail.price = cart_item.item.price
+      @order_ditail.amount = cart_item.amount
+      @order_ditail.save
     end
+    current_customer.cart_items.destroy_all
     redirect_to compleate_orders_path
   end
 
@@ -29,6 +30,14 @@ class Public::OrdersController < ApplicationController
     @order.postal_code = current_customer.postal_code
     @order.address = current_customer.address
     @order.name = current_customer.first_name + current_customer.last_name
+  end
+
+  def index
+    @order_ditails = OrderDitail.all
+  end
+
+  def show
+    @order_ditail = OrderDitail.find(params[:id])
   end
 
   private
